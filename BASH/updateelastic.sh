7f6b128a56b99728018b3827e01b93d857245b7d
Vinzel <75076070+vinzel-ops@users.noreply.github.com>
2023-12-17 17:54:26 +0700

    Update updateelastic.sh

--- BASH/updateelastic.sh
+++ BASH/updateelastic.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
-# Mengambil versi terbaru Elasticsearch dari situs resmi
-latest_version=$(wget -qO- https://www.elastic.co/downloads/past-releases#elasticsearch | grep -oP 'elasticsearch-\K[0-9]+\.[0-9]+\.[0-9]+' | head -1)
+# Ensure wget is available
+sudo apt-get install --reinstall wget
 
-# Mendapatkan versi Elasticsearch yang terinstal
-installed_version=$(elasticsearch --version | grep -oP '[0-9]+\.[0-9]+\.[0-9]+')
+# Function to add a GPG key and repository
+add_repo() {
+    key_url=$1
+    repo_info=$2
+    keyring_path=$3
+    list_path=$4
 
-echo "Versi terbaru: $latest_version"
-echo "Versi terinstal: $installed_version"
+    # Add GPG Key
+    wget -qO - $key_url | gpg --dearmor | sudo tee $keyring_path > /dev/null
 
-# Bandingkan versi dan update jika diperlukan
-if [ "$latest_version" != "$installed_version" ]; then
-    echo "Melakukan update Elasticsearch..."
-    sudo apt-get update && sudo apt-get install --only-upgrade elasticsearch
-else
-    echo "Elasticsearch sudah terupdate."
-fi
+    # Add Repository
+    echo "deb [signed-by=$keyring_path] $repo_info" | sudo tee $list_path
+}
+
+# Elasticsearch details
+es_key="https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+es_repo="https://artifacts.elastic.co/packages/8.x/apt stable main"
+es_keyring_path="/usr/share/keyrings/elasticsearch.gpg"
+es_list_path="/etc/apt/sources.list.d/elastic-8.x.list"
+
+# Kibana details
+kb_key="https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+kb_repo="https://artifacts.elastic.co/packages/8.x/apt stable main"
+kb_keyring_path="/usr/share/keyrings/kibana.gpg"
+kb_list_path="/etc/apt/sources.list.d/kibana-8.x.list"
+
+# Add Elasticsearch repository
+add_repo $es_key $es_repo $es_keyring_path $es_list_path
+
+# Add Kibana repository
+add_repo $kb_key $kb_repo $kb_keyring_path $kb_list_path
+
+# Update and Install Elasticsearch and Kibana
+sudo apt-get update
+sudo apt-get install elasticsearch kibana
